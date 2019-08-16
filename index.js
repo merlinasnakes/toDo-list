6283eb2e9b7b618a58864863390a3d9113424187
Merlina Snakes <caecilia.ae@gmail.com>
2019-08-16 03:08:03 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+
+var botonAgregarTarea = document.getElementById('agregarTarea')
+botonAgregarTarea.addEventListener('click', agregarTarea);
+
+var tareaNueva = document.getElementById('tareaNueva');
+tareaNueva.addEventListener('click', comprobarInput);
+
+
+
+
+function agregarTarea () {
+
+	var tareaUsuario = document.getElementById('tareaNueva').value
+
+
+	var nodeLi = document.createElement('li');
+
+	var textoEliminar = document.createTextNode("Eliminar Tarea");
+	var nodeButtonEliminar = document.createElement('button');
+	nodeButtonEliminar.appendChild(textoEliminar);
+
+	var nodeButtonTachar = document.createElement('button');
+	var textoTachar = document.createTextNode("Tarea Realizada")
+	nodeButtonTachar.appendChild(textoTachar);
+	
+	var textLi = document.createTextNode(tareaUsuario);
+	nodeLi.appendChild(textLi);
+	nodeLi.appendChild(nodeButtonEliminar);
+	nodeLi.appendChild(nodeButtonTachar);
+	document.getElementById('listaDeTareas').appendChild(nodeLi);
+	nodeButtonEliminar.addEventListener('click', eliminarNodo);
+	nodeButtonTachar.addEventListener('click', tacharTareaRealizada);
+
+	var inputUsuario = document.getElementById('tareaNueva')
+	inputUsuario.value = "";
+	inputUsuario.addEventListener("click", comprobarInput);
+
+	if (tareaUsuario === "") {
+		tareaNueva.setAttribute("placeholder", "Agrega una tarea válida");
+		tareaNueva.className = "error";
+		eliminarNodo()
+	}
+
+ 	function eliminarNodo () {
+ 		nodeLi.parentNode.removeChild(nodeLi);
+
+ 	}
+	
+	function tacharTareaRealizada() {
+		nodeLi.classList.toggle('tareaTachada');
+
+	};
+
+	function comprobarInput () {
+	inputUsuario.className = "";
+	inputUsuario.setAttribute("placeholder", "Agrega tu tarea");
+	};
+
+
+
+};
+
+
+
